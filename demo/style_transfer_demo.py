fa2f003e6b526013ff664f675c0b0ed9fccdbab1
Michael Jurkoic <mjjurkoic@gmail.com>
2020-04-15 20:31:50 -0700

    Demo mostly working.
    There are still some error messages to be worked out.  The tutorial was for an older version of TensorFlow, and parts of the code need to be updated.  Additionally reorganized the image folders.

--- demo/style_transfer_demo.py
+++ demo/style_transfer_demo.py
@@ -6,6 +6,7 @@
 
 
 import functools
+import IPython.display
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
@@ -16,18 +17,19 @@ from tensorflow.python.keras.preprocessing import image as kp_image
 from tensorflow.python.keras import models
 from tensorflow.python.keras import losses
 from tensorflow.python.keras import layers
-from tensorflow.python.keras import backed as K
+from tensorflow.python.keras import backend as K
 import time
 
 
-# Make sure eager execution is enabled for best performance
-tf.enable_eager_execution()
-print("Eager execution: {}".format(tf.executing_eagerly()))
+# Eager execution is not needed in TensorFlow 2.
+# # Make sure eager execution is enabled for best performance
+# tf.enable_eager_execution()
+# print("Eager execution: {}".format(tf.executing_eagerly()))
 
 
 # Global variables
-content_path = "images/"
-style_path = "images/"
+content_path = "images/content/dubrovnik.jpg"
+style_path = "images/style/canaletto.jpg"
 
 # The style transfer technique uses a pretrained image classification neural
 # network from the Oxford Visual Geometry Group (VGG).  The technique pulls the
@@ -215,10 +217,107 @@ def compute_grads(cfg):
     return tape.gradient(total_loss, cfg['init_image']), all_loss
 
 
+# Puts all the pieces together into an optimization loop.
+# The parameters here are great tweakables.
+# The tutorial uses the Adam optimizer to minimize loss.  The original paper
+# recommends using L-BFGS as an optimizer algorithm (we should try implementing
+# it and see if it makes a difference).
+# The network updates the output image to minimize loss, it doesn't update the 
+# weights in the network.
+def run_style_transfer(content_path, style_path, num_iterations=1000,\
+    content_weight=1e3, style_weight=1e-2):
+    # We don't need to train any layers of the VGG model, so set trainable to
+    # false.
+    model = get_model()
+    for layer in model.layers:
+        layer.trainable = False
+    # Get the style and content feature representations
+    style_features, content_features = get_feature_representations(model,\
+        content_path, style_path)
+    gram_style_features = [gram_matrix(style_feature) for style_feature\
+        in style_features]
+    # Set the initial image
+    init_image = load_and_process_img(content_path)
+    init_image = tf.Variable(init_image, dtype=tf.float32)
+    # Create the optimizer
+    opt = tf.optimizers.Adam(learning_rate=5, beta1=0.99, epsilon=1e-1)
+    # For displaying intermediate images
+    iter_count = 1
+    # Store the best result
+    best_loss, best_img = float('inf'), None
+    # Create a nice config
+    loss_weights = (style_weight, content_weight)
+    cfg = {'model': model,
+           'loss_weights': loss_weights,
+           'init_image': init_image,
+           'gram_style_features': gram_style_features,
+           'content_features': content_features}
+    # For displaying
+    num_rows = 2
+    num_cols = 5
+    display_interval = num_iterations / (num_rows * num_cols)
+    start_time = time.time()
+    global_start = time.time()
+    # What do these numbers do?
+    norm_means = np.array([103.939, 116.779, 123.68])
+    min_vals = -norm_means
+    max_vals = 255 - norm_means
+    imgs = []
+    for i in range(num_iterations):
+        grads, all_loss = compute_grads(cfg)
+        loss, style_score, content_score = all_loss
+        opt.apply_gradients([(grads, init_image)])
+        clipped = tf.clip_by_value(init_image, min_vals, max_vals)
+        init_image.assign(clipped)
+        end_time = time.time()
+        if loss < best_loss:
+            # Update best loss and best image from the total loss.
+            best_loss = loss
+            best_image = deprocess_img(init_image.numpy())
+        if i % display_interval == 0:
+            start_time = time.time()
+            # Use the .numpy() method to get the numpy array
+            plot_img = init_image.numpy()
+            plot_img = deprocess_img(plot_img)
+            imgs.append(plot_img)
+            IPython.display.clear_output(wait=True)
+            IPython.display.display_png(Image.fromarray(plot_img))
+            print('Iteration: {}'.format(i))
+            print('Total loss: {:.4e}, '
+                  'style loss: {:.4e}, '
+                  'content loss: {:.4e}, ' 
+                  'time: {:.4f}s'.format(loss, style_score, content_score,\
+                      time.time() - start_time))
+    print('Total time: {:.4f}s'.format(time.time() - global_start))
+    IPython.display.clear_output(wait=True)
+    plt.figure(figsize=(14,4))
+    for i,img in enumerate(imgs):
+        plt.subplot(num_rows, num_cols, i+1)
+        plt.imshow(img)
+        plt.xticks([])
+        plt.yticks([])
+    return best_img, best_loss
+
+
+def show_results(best_img, content_path, style_path, show_large_final=True):
+    plt.figure(figsize=(10, 5))
+    content = load_img(content_path)
+    style = load_img(style_path)
+    plt.subplot(1, 2, 1)
+    imshow(content, 'Content Image')
+    plt.sublplot(1, 2, 2)
+    imshow(style, 'Style Image')
+    if show_large_final:
+        plt.figure(figsize=(10, 10))
+        plt.imshow(best_img)
+        plt.title('Output Image')
+        plt.show()
+
+
 def main():
-    # Get an image for content and an image for style
-    content = load_img(content_path).astype('uint8')
-    style = load_img(style_path).astype('uint8')
+    best, best_loss = run_style_transfer(content_path, style_path,\
+        num_iterations=1000)
+    show_results(best, content_path, style_path)
 
 
 if __name__ == "__main__":
